a96d86714264fef59ac14a620c9bbedd7fec1ac9
ChristopherVuvu <vuvumumo93@gmail.com>
2023-04-26 18:58:32 +0300

    added

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,7 +6,8 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray=[...foods, newFood]
+    setFoods(newFoodArray)
   }
 
   const foodList = foods.map((food) => (
@@ -14,6 +15,22 @@ function SpicyFoodList() {
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
+
+  function handleLiClick(id) {
+    //const newFoodArray = foods.filter((food) => food.id !== id);
+    //setFoods(newFoodArray);
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
 
   return (
     <div>
